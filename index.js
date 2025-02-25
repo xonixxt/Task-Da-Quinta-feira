1446d12a7e6e5f3db64753b86319961293924685
xonixxt <jota.bbiel@gmail.com>
2025-02-25 15:02:50 -0300

    dia add

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+let dia = new Date().getDay();
+document.getElementById(`dia`).textContent = `hoje é: ${dia}`;
+
+switch(dia)
+{   
+    case 0:
+    document.getElementById(`dia`).textContent = `hoje é Domingo`
+    break;
+
+    case 1:
+        document.getElementById(`dia`).textContent = `hoje é Segunda-Feira`
+        break;
+
+    case 2:
+        document.getElementById(`dia`).textContent = `hoje é Terça-Feira`
+        break;
+
+    case 3:
+        document.getElementById(`dia`).textContent = `hoje é Quarta-Feira`
+        break;
+
+    case 4:
+        document.getElementById(`dia`).textContent = `hoje é Quinta-Feira`
+        break;
+
+        case 5:
+        document.getElementById(`dia`).textContent = `hoje é Sexta-Feira`
+        break;
+
+        case 6:
+        document.getElementById(`dia`).textContent = `hoje é Sábado`
+        break;
+
+    default: document.getElementById(`dia`).textContent = `Error`;
+    break;
+} 
+            
+
+    
+
 function mudarCor(){
    let cor = document.getElementById(`selecionarcor`).value.toLowerCase();
     switch (cor){
@@ -32,6 +72,7 @@ function mudarCor(){
     }
 }
 
+
 function limparpos(){
     document.getElementById(`selecionarcor`).value = " ";
 }
@@ -47,3 +88,4 @@ document.getElementById(`selecionarcor`).onfocus = function() {
     document.getElementById(`selecionarcor`).value = ``;
     document.getElementById("resultadocor").textContent = ``;
 }
+
